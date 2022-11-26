28946a0af75fe3b8045a93457be62d385a65afa8
DaniyalSyed24 <daniyalv.syed@gmail.com>
2022-11-26 15:44:11 +0000

    fixed backend errors

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,13 @@
 const express = require('express');
-var createError = require('http-errors');
 const cookieParser = require('cookie-parser');
 var path = require('path');
 const logger = require('morgan');
 const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var StudentIDsRouter = require('./routes/StudentIDs');
+var indexRouter = require('./routes/index'); //
+var StudentIDsRouter = require('./routes/StudentIDs'); //
 
 const app = express();
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,21 +15,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 
-app.use('/', indexRouter);
-app.use('/StudentIDs', StudentIDsRouter);
+app.use('/StudentIDs', StudentIDsRouter); //
 
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.get('/', (req, res, next) => {
+  res.send('hello world')
+})
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 module.exports = app;
