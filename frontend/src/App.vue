db76aef2da21d2e6027ef96140352b2ac46a754a
DaniyalSyed24 <daniyalv.syed@gmail.com>
2022-11-27 04:59:33 +0000

    styled view background

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,29 @@
 <template>
   <nav> <!-- this is for links -->
-    <router-link to="/">Attendance Register</router-link> |
+    <!-- <router-link to="/">Attendance Register</router-link> -->
     <!-- <router-link to="/about">About</router-link> -->
   </nav>
   <router-view/>
 </template>
+
+
+<style>
+#app{
+  background: linear-gradient(to left,#9c88ff,#3cabeb);
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  
+  text-align: center;
+  color: black;
+};
+
+nav {
+  padding: 30px;
+}
+
+a {
+  font-weight: bold;
+}
+</style>
 
