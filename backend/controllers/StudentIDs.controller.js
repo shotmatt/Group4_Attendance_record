d4770639515d78719e5857c7484b4d74dce1d23b
DaniyalSyed24 <daniyalv.syed@gmail.com>
2022-11-29 16:11:40 +0000

    created a findall function

--- backend/controllers/StudentIDs.controller.js
+++ backend/controllers/StudentIDs.controller.js
@@ -1,16 +1,18 @@
-const db = require("../models/StudentIDs.model");
+//const db = require("../models/StudentIDs.model");
+const db = require("../models");
 const StudentID = db.StudentIDs;
 
-var querystring = require("querystring");
+//var querystring = require("querystring");
 
-//Welcome page
-exports.start = (response) => {
-    response.writeHead(200, {"Content-type": "text/plain"});
-    response.write("Welcome to the StudentID system");
-    response.end();
-};
+////Welcome page
+//exports.start = (response) => {
+//    response.writeHead(200, {"Content-type": "text/plain"});
+//    response.write("Welcome to the StudentID system");
+//    response.end();
+//};
 
 // Find a single Animal with an id
+
 exports.findOne = (req, res) => {
     const Name = req.params.Name;
 
@@ -18,36 +20,72 @@ exports.findOne = (req, res) => {
         .then(data => {
             if (!data)
                 res.status(404).send({ message: "Not found Student with name: " + Name});
-            else 
+            else
                 res.send(data);
         })
         .catch(err => {
             res.status(500).send({message: "Error retriving Student with name: " + Name});
         })
- 
+
 };
- 
+
+//exports.update = (req, res) => {
+//    if (!req.body) {
+//        return res.status(400).send({
+//            message: "Data to update can not be empty!"
+//        });
+//    }
+
+//    const name = req.params.name;
+
+//    StudentIDs.findByNameAndUpdate(name, req.body, { useFindAndModify: false })
+//        .then(data => {
+//            if (!data) {
+//                res.status(404).send({
+//                    message: `Cannot update Student with name=${name}. Maybe Student was not found!`
+//                });
+//            } else
+//                res.send({ message: "Student was updated successfully." });
+//        })
+//        .catch(err => {
+//            res.status(500).send({
+//                message: "Error updating Student with name=" + name
+//            });
+//        });
+//};
+
+//create and save a new student
+exports.create = (req, res) => {
+
+};
+
+exports.findAll = (req, res) => {
+    const name = req.query.name;
+    //console.log(StudentID.findAll);
+    var condition = name ? { name: { $regex: new RegExp(name), $options: "i" } } : {};
+    console.log(condition);
+    StudentID.find(condition).then(data => {
+        //res.render("students", { title: "Student IDs", students: data });
+        console.log("findin all");
+        console.log(data);
+        res.json(data);
+    }).catch(err => {
+        res.status(500).send({ message: err.message || "error while retrieving StudentIDs" });
+    });
+};
+
+//exports.findOne = (req, res) => {
+
+//};
+
 exports.update = (req, res) => {
-    if (!req.body) {
-        return res.status(400).send({
-            message: "Data to update can not be empty!"
-        });
-    }
 
-    const name = req.params.name;
+};
 
-    StudentIDs.findByNameAndUpdate(name, req.body, { useFindAndModify: false })
-        .then(data => {
-            if (!data) {
-                res.status(404).send({
-                    message: `Cannot update Student with name=${name}. Maybe Student was not found!`
-                });
-            } else 
-                res.send({ message: "Student was updated successfully." });
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: "Error updating Student with name=" + name
-            });
-        });
+exports.delete = (req, res) => {
+
+};
+
+exports.deleteAll = (req, res) => {
+
 };
--- backend/routes/StudentIDs.routes.js
+++ backend/routes/StudentIDs.routes.js
@@ -4,12 +4,16 @@ var router = express.Router();
 //Require controller
 var StudentIDsController = require('../controllers/StudentIDs.controller');
 
+//get at root
 router.get('/', function(req, res, next) {
     res.json({message: "Welcome to the Student database"});
 });
 
 // Retrieve a single Student with name
-router.get("../models/StudentIDs/:name", StudentIDsController.findOne);
+router.get("/StudentIDs/:name", StudentIDsController.findOne);
+
+//get all students
+router.get("/StudentIDs/", StudentIDsController.findAll);
  
 // Update a Student with id
 router.put("../models/StudentIDs/:name", StudentIDsController.update);
